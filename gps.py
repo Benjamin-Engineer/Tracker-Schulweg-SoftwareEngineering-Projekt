19751a0749ef55cc5ab520307a2909474be247f9
Benjamin-Engineer <benjaminbec10@gmail.com>
2025-05-19 18:00:13 +0100

    Erfassen der GPS-Daten

--- gps.py
+++ gps.py
@@ -1,19 +1,18 @@
 import serial
-import sys
+import pynmea2
 
 gps = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
 GPPGA_buffer = 0
 NMEA_buffer = 0
 
 
-
-
 while True:
              
     received_data = gps.readline().decode('ascii', errors="replace")
-    GPPGGA_DATA = received_data.find('$GPGGA,')
+    GPGGA_DATA = received_data.find('$GPGGA,')
+    GPRMC_DATA = received_data.find('$GPRMC,')
     #print(GPPGGA_DATA)
-    if GPPGGA_DATA == 0:
+    if GPGGA_DATA == 0:
         GPGGA_buffer = received_data.split('$GPGGA,',1)[1]
         NMEA_buffer = GPGGA_buffer.split(',')
         
@@ -24,10 +23,15 @@ while True:
         NMEA_lon_raw = 0
         
         NMEA_time = NMEA_buffer[0] #extracts time for NMEA-Data
-        print(NMEA_time)
         
-        NMEA_lat = NMEA_buffer[1]
-        NMEA_lon = NMEA_buffer[3]
+        msg_local = pynmea2.parse(received_data)
         
-        print('Latraw: ', NMEA_lat, ' Lonraw: ',NMEA_lon)
+        lat = msg_local.latitude
+        lon = msg_local.longitude
         
+        
+    elif GPRMC_DATA == 0:
+        msg_chrono = pynmea2.parse(received_data)
+        
+        date = msg_chrono.datestamp
+        time = msg_chrono.timestamp
