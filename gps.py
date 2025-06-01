844a2f146396c5f87c92a64382c2a02d505e6df8
Benjamin-Engineer <benjaminbec10@gmail.com>
2025-06-01 18:23:23 +0100

    Anpassen von Start/Stop über den Planner und starten des Trackings nach setzen des start buttons auf True

--- gps.py
+++ gps.py
@@ -3,45 +3,35 @@ import pynmea2
 from dateifunktionen import gps_json_write
 from datetime import datetime
 
-from start_stop import is_recording
-
 starttime = str(datetime.now()) # Startzeit des Programmes
 
 gps = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
 
 def tracker():
-    while is_recording == True:
-        systime = str(datetime.now())
+    systime = str(datetime.now())
 
-        received_data = gps.readline().decode('ascii', errors="replace")
-        GPGGA_DATA = received_data.find('$GPGGA,')
-        GPRMC_DATA = received_data.find('$GPRMC,')
+    received_data = gps.readline().decode('ascii', errors="replace")
+    GPGGA_DATA = received_data.find('$GPGGA,')
+    GPRMC_DATA = received_data.find('$GPRMC,')
 
-        if GPGGA_DATA == 0:
+    if GPGGA_DATA == 0:
 
-            msg_local = pynmea2.parse(received_data)
+        msg_local = pynmea2.parse(received_data)
 
-            lat = str(msg_local.latitude)
-            lon = str(msg_local.longitude)
-
-            #print(lat, lon)
+        lat = str(msg_local.latitude)
+        lon = str(msg_local.longitude)
 
 
-        elif GPRMC_DATA == 0:
+    elif GPRMC_DATA == 0:
 
-            msg_chrono = pynmea2.parse(received_data)
+        msg_chrono = pynmea2.parse(received_data)
 
-            date = str(msg_chrono.datestamp)
-            time = str(msg_chrono.timestamp)
-
-            #print(date, time)
-
+        date = str(msg_chrono.datestamp)
+        time = str(msg_chrono.timestamp)
 
         
-        try:
-            coord = lat + " " + lon
-            gps_json_write(coord, systime, date, starttime) # Koordinaten, Zeit, Ordner, Dateiname
-        except:
-            gps_json_write("NO SIGNAL", systime, "Error Log", starttime) # Meldung, Zeit, Ordner, Dateiname
-
-tracker()
+    try:
+        coord = lat + " " + lon
+        gps_json_write(coord, systime, date, starttime) # Koordinaten, Zeit, Ordner, Dateiname
+    except:
+        gps_json_write("NO SIGNAL", systime, "Error Log", starttime) # Meldung, Zeit, Ordner, Dateiname
